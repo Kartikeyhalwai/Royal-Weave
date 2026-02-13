eb7abb1db38f4dfbe46d25ca0c5be1a51cf69150
Kartikeyhalwai <halwaikartikeya@outlook.com>
2026-02-13 14:19:19 +0530

    background scroll fix when menu open, fix for menu doesnt close after clicking a link

--- js/responsive-nav.js
+++ js/responsive-nav.js
@@ -4,8 +4,21 @@
 
   openMenu.addEventListener("click", () => {
     navMenu.classList.add("active");
+    document.body.classList.add("menu-open");   // fix for background scrolls when menu open
   });
 
   closeMenu.addEventListener("click", () => {
     navMenu.classList.remove("active");
+    document.body.classList.remove("menu-open");
+
+    // fix for menu doesnt close after clicking a link 
+    
   });
+
+  const navLinks = document.querySelectorAll('.nav-links a');
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navMenu.classList.remove('active');
+    document.body.classList.remove('menu-open');
+  });
+});
